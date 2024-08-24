8526a4fd430f2ef2dc83bfba7f665291b23ae7e0
NguyenAnhDuc <anhduc000321@gmail.com>
2024-08-24 21:52:49 +0700

    feat: send mail

--- refactor-code/mevabe/src/main/java/com/anhduc/mevabe/service/EmailService.java
+++ refactor-code/mevabe/src/main/java/com/anhduc/mevabe/service/EmailService.java
@@ -5,6 +5,7 @@ import jakarta.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring6.SpringTemplateEngine;
@@ -21,6 +22,7 @@ public class EmailService {
     @Autowired
     private SpringTemplateEngine templateEngine;
 
+    @Async
     public void sendEmail(String to, String subject, String templateName, Map<String, Object> variables) {
         try {
             MimeMessage message = javaMailSender.createMimeMessage();
